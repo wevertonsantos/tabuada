c17ffff27dce1416708e75e1e061c7dac3e08912
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-29 17:38:15 -0300

    exibindo a tabuada

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,29 @@
 const multiplicationForm = document.querySelector("#multiplication-form")
 const numberInput = document.querySelector("#number")
 const multiplicationInput = document.querySelector("#multiplicator")
+const multiplicationTitle = document.querySelector("#multiplication-title span")
+const multiplicationTable = document.querySelector("#multiplication-operations")
 
 // Funções
+const createTable = (number,multiplicatorNumber) => {
+    multiplicationTable.innerHTML = ""
+
+    for (i = 1; i <= multiplicatorNumber; i++){
+        const result = number * i
+        const template = `
+        <div class="row">
+            <div class="operation">${number} x ${i} =</div>
+            <div class="result">${result}</div>
+        </div>
+        `  
+        const parser = new DOMParser()
+        const htmlTemplate = parser.parseFromString(template, "text/html")
+        const row = htmlTemplate.querySelector(".row")
+        multiplicationTable.appendChild(row)
+    }
+
+    multiplicationTitle.innerText = number
+}
 
 // Eventos 
 multiplicationForm.addEventListener("submit", (e) => {
@@ -14,5 +35,5 @@ multiplicationForm.addEventListener("submit", (e) => {
 
     if (!multiplicationNumber || !multiplicatorNumber) return
 
-    console.log(multiplicationNumber,multiplicatorNumber)
+    createTable(multiplicationNumber,multiplicatorNumber)
 })
